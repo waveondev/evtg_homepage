12932482068b744e489cd15e4d0e571ec2a8d554
BY.Jeon <by.jeon@waveon.kr>
2026-04-06 18:03:27 +0900

    CORS 문제 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 여기에 바로 넣으세요 (experimental 안이 아닙니다!)
+  allowedDevOrigins: ['192.168.0.42'],
+
   typescript: {
     ignoreBuildErrors: true,
   },
